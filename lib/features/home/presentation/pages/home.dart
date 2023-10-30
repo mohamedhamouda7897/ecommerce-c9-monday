a150a03b1c68e1166045e5d44afb827d83941123
Mohammedzxz712 <bdallhlym712@gmail.com>
2023-10-30 18:23:46 +0200

    homeUI done

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -1,11 +1,280 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-// mohamed here
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  List<String> imageUrls = [
+    "https://via.placeholder.com/398x200",
+    "https://via.placeholder.com/398x200",
+    "https://via.placeholder.com/398x200",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leadingWidth: 83.w,
+        leading: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Image.asset('assets/images/route.png'),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(vertical: 5.h),
+                          border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Color(0xFF004182)),
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Color(0xFF004182)),
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Color(0xFF06004E),
+                          ),
+                          hintText: 'what do you search for?',
+                          hintStyle: TextStyle(
+                            color: Color(0x9906004E),
+                            fontSize: 14.sp,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w300,
+                          )),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Icon(
+                    Icons.shopping_cart,
+                    size: 30.0.sp,
+                    color: Color(0xff004182),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 16.h,
+            ),
+            Stack(
+              children: [
+                CarouselSlider(
+                  options: CarouselOptions(
+                    height: 200.h,
+                    aspectRatio: 16 / 9,
+                    viewportFraction: 0.93,
+                    initialPage: 0,
+                    enableInfiniteScroll: true,
+                    reverse: false,
+                    enlargeCenterPage: true,
+                    enlargeFactor: 0.2,
+                    scrollDirection: Axis.horizontal,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        _currentIndex = index;
+                      });
+                    },
+                  ),
+                  items: imageUrls.map((imageUrl) {
+                    return Container(
+                      width: double.infinity,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: Image.network(
+                          imageUrl,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+                Positioned(
+                  bottom: 20.h,
+                  left: 0,
+                  right: 0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: imageUrls.asMap().entries.map((entry) {
+                      int index = entry.key;
+                      return Container(
+                        width: 10.w,
+                        height: 10.h,
+                        margin: EdgeInsets.symmetric(horizontal: 4.w),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _currentIndex == index
+                              ? Color(0xFF06004E)
+                              : Colors.white,
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 16.w),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Categories',
+                    style: TextStyle(
+                      color: Color(0xFF06004E),
+                      fontSize: 18.sp,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'view all',
+                      style: TextStyle(
+                        color: Color(0xFF06004E),
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            catItem(),
+            catItem(),
+            Padding(
+              padding: EdgeInsets.only(left: 16.w),
+              child: Text(
+                'Home Appliance',
+                style: TextStyle(
+                  color: Color(0xFF06004E),
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            homeApp(),
+            SizedBox(
+              height: 20,
+            )
+          ],
+        ),
+      ),
+    );
   }
+
+  Widget catItem() => Container(
+        height: 144.h,
+        margin: EdgeInsets.only(left: 16.w),
+        child: ListView.separated(
+            separatorBuilder: (context, index) => SizedBox(
+                  width: 16.w,
+                ),
+            scrollDirection: Axis.horizontal,
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  Container(
+                    width: 100.w,
+                    height: 100.h,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image:
+                            NetworkImage("https://via.placeholder.com/100x100"),
+                        fit: BoxFit.cover,
+                      ),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8.h,
+                  ),
+                  Text(
+                    'Item ',
+                    style: TextStyle(
+                      color: Color(0xFF06004E),
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              );
+            }),
+      );
+  Widget homeApp() => Container(
+        margin: EdgeInsets.only(left: 16.w, top: 16.h),
+        height: 122.h,
+        child: ListView.separated(
+            separatorBuilder: (context, index) => SizedBox(
+                  width: 16.w,
+                ),
+            scrollDirection: Axis.horizontal,
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Container(
+                width: 158.w,
+                height: 122.h,
+                padding: EdgeInsets.only(
+                  top: 8.h,
+                  left: 134.w,
+                  right: 8.w,
+                  bottom: 98.h,
+                ),
+                clipBehavior: Clip.antiAlias,
+                decoration: ShapeDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage("https://via.placeholder.com/158x122"),
+                    fit: BoxFit.fill,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                    ),
+                  ),
+                ),
+                child: Container(
+                  width: 16.w,
+                  height: 16.h,
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: OvalBorder(),
+                    shadows: [
+                      BoxShadow(
+                        color: Color(0x26000000),
+                        blurRadius: 10,
+                        offset: Offset(0, 5),
+                        spreadRadius: 0,
+                      )
+                    ],
+                  ),
+                  child: Icon(
+                    Icons.favorite_border,
+                    size: 12.sp,
+                  ),
+                ),
+              );
+            }),
+      );
 }
